bf03e7a7c868ed9954a40734d0eaf1361a6f06b2
ten30 <ten303@hotmail.com>
2017-03-17 20:29:56 +0700

    Initial commit

---
+++ app/src/main/java/com/tenjirawat/liveat500px/MainApplication.java
@@ -0,0 +1,24 @@
+package com.tenjirawat.liveat500px;
+
+import android.app.Application;
+
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
+/**
+ * Created by ten30 on 3/17/2017.
+ */
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Contextor.getInstance().init(getApplicationContext());
+
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
